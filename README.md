77620cde7127520dd83e6ff5d61aa55327b28b8b
william_doyle <mcbidebilly@gmail.com>
2023-11-15 18:14:06 -0500

    style nav

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 
 
-Test cases:
-http://localhost:3000/address/0xd90f7Fb941829CFE7Fc50eD235d1Efac05c58190
+Format
+    
+    /address/{address}
+    /tx/{transaction hash}
 
-http://localhost:3000/tx/0x5a2e936f418affd295fcd91e8bbbbcc04ad58451bed856d4cf5b2413bd270a72
+Test cases:
+    
+    http://localhost:3000/address/0xd90f7Fb941829CFE7Fc50eD235d1Efac05c58190
+
+    http://localhost:3000/tx/0x5a2e936f418affd295fcd91e8bbbbcc04ad58451bed856d4cf5b2413bd270a72
--- src/App.tsx
+++ src/App.tsx
@@ -15,30 +15,36 @@ import TransactionDetails from './TransactionDetails';
 function App() {
   return (
     <Router>
-      <div>
+      <div className="app-container">
+        <header className="app-header">
+          <h1>Ethereum & Polygon Combined Explorer</h1>
+        </header>
+
         {/* Links to navigate */}
-        <ul>
-          <li>
-            <Link to="/address">Address</Link>
-          </li>
-          <li>
-            <Link to="/tx">Transaction</Link>
-          </li>
-        </ul>
+        <nav className="app-nav">
+          <ul>
+            <li>
+              <Link to="/address">Address</Link>
+            </li>
+            <li>
+              <Link to="/tx">Transaction</Link>
+            </li>
+          </ul>
+        </nav>
 
         {/* Route configuration */}
-        <Routes>
-          <Route path="/address/:address_input" element={<AddressDetails />} />
-          <Route path="/address/" element={<Navigate to="/address/0xd90f7Fb941829CFE7Fc50eD235d1Efac05c58190" />} />
+        <main className="app-main">
+          <Routes>
+            <Route path="/address/:address_input" element={<AddressDetails />} />
+            <Route path="/address/" element={<Navigate to="/address/0xd90f7Fb941829CFE7Fc50eD235d1Efac05c58190" />} />
 
-          <Route path="/tx/:tx_input" element={<TransactionDetails />} />
-          <Route path="/tx/" element={<Navigate to="/tx/0x5a2e936f418affd295fcd91e8bbbbcc04ad58451bed856d4cf5b2413bd270a72" />} />
-        </Routes>
+            <Route path="/tx/:tx_input" element={<TransactionDetails />} />
+            <Route path="/tx/" element={<Navigate to="/tx/0x5a2e936f418affd295fcd91e8bbbbcc04ad58451bed856d4cf5b2413bd270a72" />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
 }
-
-
 
 export default App;
